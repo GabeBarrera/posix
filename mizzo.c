6cc32bee55aa607ac587f8e763d2490184db520a
Gabe Barrera <gsbarrera1@gmail.com>
2019-04-10 10:04:13 -0700

    getopt added

--- mizzo.c
+++ mizzo.c
@@ -30,81 +30,41 @@ bool numcheck(char ** argv, int i){
  * 
  * Executes pagetable with parameters.
  */
-int main(int argc, char ** argv){
-    int i = 0; // Argument counter
-    int j = 0; // lvlBits counter
-    int ethl = 0; // Ethels milliseconds
-    int lucy = 0; // Lucys milliseconds
-    int fval = 0; // Frog Bite milliseconds
-    int eval = 0; // Escargo Sucker milliseconds
-    bool ethlf = false; // Memory set flag
-    bool lucyf = false; // Export file flag
-    bool fflag = false; // Physical address location
-    bool eflag = false;
-    
-    // Args 1 to 5 contain the max amount of flag args 
-    for(i=0; i < argc; i++){
-        // -E FLAG
-        if (strcmp(argv[i],"-E") == 0){
-            if (argv[i+1]!=NULL){
-                if (numcheck(argv,i+1)){
-                    ethl = atoi(argv[i+1]);
-                    ethlf = true;
-                    i++;
-                }
-                else printf("** ERR: Non-integer, -E flag using NO DELAY\n");
-            }
-
-            else printf("** ERR: No arg, -E flag using NO DELAY\n");
-        }
-
-        // -L FLAG
-        if (strcmp(argv[i],"-L") == 0){
-            if (argv[i+1]!=NULL){
-                if (numcheck(argv,i+1)){
-                    lucy = atoi(argv[i+1]);
-                    lucyf = true;
-                    i++;
-                }
-                else printf("** ERR: Non-integer, -L flag using NO DELAY\n");
-            }
-
-            else printf("** No arg, -L flag using NO DELAY\n");
-        }
-
-        // -f FLAG
-        if (strcmp(argv[i],"-f") == 0){
-            if (argv[i+1]!=NULL){
-                if (numcheck(argv,i+1)){
-                    fval = atoi(argv[i+1]);
-                    fflag = true;
-                    i++;
-                }
-                else printf("** ERR: Non-integer, -f flag using NO DELAY\n");
-            }
-
-            else printf("** ERR: No arg, -f flag using NO DELAY\n");
-        }
-
-        // -e FLAG
-        if (strcmp(argv[i],"-e") == 0){
-            if (argv[i+1]!=NULL){
-                if (numcheck(argv,i+1)){
-                    eval = atoi(argv[i+1]);
-                    eflag = true;
-                    i++;
-                }
-                else printf("** ERR: Non-integer, -e flag using NO DELAY\n");
-            }
-
-            else printf("** ERR: No arg, -e flag using NO DELAY\n");
-        }
+int main(int argc, char * argv[]){
+    int opt; 
+    int ethl = 0;
+    int lucy = 0;
+    int fval = 0;
+    int eval = 0;  
+    // put ':' in the starting of the 
+    // string so that program can  
+    //distinguish between '?' and ':'  
+    while((opt = getopt(argc, argv, ":if:lrx")) != -1)  
+    {  
+        switch(opt)  
+        {  
+            case 'E':  
+            case 'L':  
+            case 'e':  
+                printf("option: %c\n", opt);  
+                break;  
+            case 'f':  
+                printf("filename: %s\n", optarg);  
+                break;  
+            case ':':  
+                printf("option needs a value\n");  
+                break;  
+            case '?':  
+                printf("unknown option: %c\n", optopt); 
+                break;  
+        }  
+    }  
+      
+    // optind is for the extra arguments 
+    // which are not parsed 
+    for(; optind < argc; optind++){      
+        printf("extra arguments: %s\n", argv[optind]);  
     }
-
-    printf("\nETHEL: %d ms\n", ethl);
-    printf("LUCY: %d ms\n", lucy);
-    printf("FROG: %d ms\n", fval);
-    printf("ESCARGO: %d ms\n", eval);
 
     return 0;
 }
